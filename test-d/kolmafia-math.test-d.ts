645e0bcd7e830ab568f9ecdb4c3d55e0fb28ff01
pastelmind <keepyourhonor@gmail.com>
2021-06-23 12:32:10 +0900

    test: Reorganize tests for `min()` and `max()`
    When tests for math functions were added to `kolmafia-math.test-d.ts`,
    existing tests for `min()` and `max()` in `kolmafia.test-d.ts` were
    overlooked. Let's move these tests into `kolmafia-math.test-d.ts`.

--- test-d/kolmafia-math.test-d.ts
+++ test-d/kolmafia-math.test-d.ts
@@ -22,8 +22,26 @@ expectType<number>(logN(5, 5.5));
 expectType<number>(logN(5));
 expectError(logN(5, undefined));
 
-expectType<number>(max(0, 1, 2));
-expectType<number>(min(0, 1, 2));
+expectType<number>(max(0));
+expectType<number>(max(0, 1));
+expectType<number>(max(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15));
+
+// @ts-expect-error In tsd 0.14.0, expectError() doesn't catch this error
+max();
+expectError(max('foo bar'));
+expectError(max(null));
+expectError(max(undefined));
+
+expectType<number>(min(0));
+expectType<number>(min(0, 1));
+expectType<number>(min(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15));
+
+// @ts-expect-error In tsd 0.14.0, expectError() doesn't catch this error
+min();
+expectError(min('foo bar'));
+expectError(min(null));
+expectError(min(undefined));
+
 expectType<number>(random(10));
 expectType<number>(round(1.5));
 expectType<number>(squareRoot(100));
