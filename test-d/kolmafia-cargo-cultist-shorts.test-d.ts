e1ecfa069879b95fb3c213b2662a5fcfd482f6d7
pastelmind <keepyourhonor@gmail.com>
2021-07-13 14:27:49 +0900

    feat!: update cargo shorts functions
    - Add JSDoc comments for all functions that examine and manipulate
      pockets in the Cargo Cultist Shorts.
    
    BREAKING CHANGE:
    
    - Fix the return type of variants of `pickPocket()` that accept an
      `Effect`, `Item`, or `Stat`.
      Note that these functions work in JS but are bugged in ASH as of
      r20797. Their signatures may change in the future.

--- test-d/kolmafia-cargo-cultist-shorts.test-d.ts
+++ test-d/kolmafia-cargo-cultist-shorts.test-d.ts
@@ -35,23 +35,23 @@ expectType<number>(availablePocket(Item.get('foo')));
 expectType<number>(availablePocket(Stat.get('foo')));
 
 expectType<Record<number, true>>(effectPockets());
-expectType<Record<number, boolean>>(itemPockets());
-expectType<Record<number, boolean>>(jokePockets());
+expectType<Record<number, true>>(itemPockets());
+expectType<Record<number, true>>(jokePockets());
 expectType<Record<number, number>>(meatPockets());
-expectType<Record<number, boolean>>(monsterPockets());
+expectType<Record<number, true>>(monsterPockets());
 expectType<Record<number, number>>(poemPockets());
-expectType<Record<number, boolean>>(restorationPockets());
+expectType<Record<number, true>>(restorationPockets());
 expectType<Record<number, number>>(scrapPockets());
-expectType<Record<number, boolean>>(statsPockets());
+expectType<Record<number, true>>(statsPockets());
 
-expectType<Record<number, boolean>>(pickedPockets());
-expectType<Record<number, boolean>>(pickedScraps());
+expectType<Record<number, true>>(pickedPockets());
+expectType<Record<number, true>>(pickedScraps());
 
 expectType<boolean>(pickPocket(1234));
 expectType<boolean>(pickPocket(Monster.get('foo')));
-expectType<Record<string, number>>(pickPocket(Effect.get('foo')));
-expectType<Record<string, number>>(pickPocket(Item.get('foo')));
-expectType<Record<string, number>>(pickPocket(Stat.get('foo')));
+expectType<boolean>(pickPocket(Effect.get('foo')));
+expectType<boolean>(pickPocket(Item.get('foo')));
+expectType<boolean>(pickPocket(Stat.get('foo')));
 
 expectType<Record<string, number>>(pocketEffects(123));
 expectType<Record<string, number>>(pocketItems(123));
